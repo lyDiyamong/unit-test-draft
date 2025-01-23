dd334cb429f5e54bf5a84295d12c07f189c7996a
lyDiyamong <diyamong26@gmail.com>
2025-01-23 16:10:03 +0700

    conf: add verbose of the the result

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,8 @@ const config: import("ts-jest").JestConfigWithTsJest = {
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
     // Match files with .test.ts or .spec.ts extensions
     testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+    // Show all test results, including passed tests
+    verbose: true, // <---- Add this line
 };
 
 export default config;
